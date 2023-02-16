0cb10ec326da3bd24f20bb939bf4c5f832654c28
lameck2207 <mokualameck66@gmail.com>
2023-02-16 06:55:51 -0800

    kii

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,14 @@
-#include<stdio.h>
-#include <unistd.h>
-/**
- * main -Entry
- * Return: 1
- */
-int main(void)
-{
-        write(1, "and a piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-        return (1);
-}
+#include <unistd.h> 
+  
+ /** 
+  * main - Entry point 
+  * 
+  * Return: Always 0 (Success) 
+  */ 
+  
+ int main(void) 
+ { 
+         write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 
+         sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19")); 
+         return (1); 
+ }
