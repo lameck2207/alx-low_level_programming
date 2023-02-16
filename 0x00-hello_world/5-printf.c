0001d71ec58ca919768e345ffe8a7d7567b4f15d
lameck2207 <mokualameck66@gmail.com>
2023-02-16 01:21:05 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - Entery point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of ar,\n");
+	return (0);
+}
