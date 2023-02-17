d73c63a6c3027b044c3f91ca9e3795c09e2314f8
lameck2207 <mokualameck66@gmail.com>
2023-02-17 00:43:08 -0800

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	int d, p;
+	for (d = '0'; d < '9'; d++)
+	{
+		for (p = d + ; p <= '9'; p++)
+	}
+}
