351952a7e77770dc6e0bf75035cad9c2dca4b661
lameck2207 <mokualameck66@gmail.com>
2023-02-17 00:19:04 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+  * main - entry point for the program
+  * Return: returns 0 if program executed successfully
+  */
+int main(void)
+{
+	printf("0123456789\n");
+	return (0);
+}
