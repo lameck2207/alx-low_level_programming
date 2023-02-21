758b7435f1947aa43f0a6e518cc19c0c2153bdc1
lameck2207 <mokualameck66@gmail.com>
2023-02-20 20:42:30 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - print _putchar
+ * is writtern by esere cynthia
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char *sh = "_putchar";
+while (*sh)
+{
+_putchar(*sh);
+sh++;
+}
+_putchar('\n');
+return (0);
+}
